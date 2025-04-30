3fcc43bc7eb7aa163f9bcf49be3db20b659d8598
pablomc20 <pablocm1747@gmail.com>
2025-04-30 15:03:31 -0600

    [UPD] - Se corrige un issue

--- src/main/java/com/hls/springboot_app/service/UserService.java
+++ src/main/java/com/hls/springboot_app/service/UserService.java
@@ -2,13 +2,15 @@ package com.hls.springboot_app.service;
 
 import com.hls.springboot_app.model.User;
 import com.hls.springboot_app.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
-    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     private UserRepository userRepository;
 
     public User saveUser(User user) {
